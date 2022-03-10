eb86eee0217ae0b97ce553e1bd1e9283610cd50f
Emma <99513625+emmadev1@users.noreply.github.com>
2022-03-10 11:37:59 +0100

    Update mimir.py

--- mimir.py
+++ mimir.py
@@ -1,8 +1,10 @@
 #Needs xprintidle package
 
 import subprocess
+import time
 
 while True:
     idle = subprocess.check_output("xprintidle")
     if int(idle) >= 1200000: #Current time is 20 minutes measured in milliseconds
         subprocess.run(["shutdown", "-P", "now"])
+    time.sleep(5)
